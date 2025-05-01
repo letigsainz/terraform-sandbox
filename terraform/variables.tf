a08928efe6e8fa5750ae3df95d1924b87f6a3288
letigsainz <18451279+letigsainz@users.noreply.github.com>
2025-05-01 15:40:49 -0400

    updating tf command flags

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,8 @@ variable "remote_state_bucket" {
   description = "The remote state bucket name. Marked as sensitive due to public repo."
   sensitive   = true
 }
+
+variable "region" {
+  type = string
+  description = "The AWS region to use."
+}
